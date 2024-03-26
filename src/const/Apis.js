6f72b3f4ae64a8fb27b63995bcef15ad9be579d1
Tushar-goel1 <141477859+Tushar-goel1@users.noreply.github.com>
2024-03-26 10:24:25 +0530

    API Change

--- src/const/Apis.js
+++ src/const/Apis.js
@@ -3,7 +3,7 @@ const Youtube_Video_API=
 `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${API_KEY}`
 
 export const YOUTUBE_SEARCH_API=
-`https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=`;
+`https://clients1.google.com/complete/search?client=youtube&gs_ri=youtube&ds=yt&q=`
 
 export const YOUTUBE_SEARCH_Video=`https://youtube.googleapis.com/youtube/v3/search?key=${API_KEY}`
 
